f04ba21ba5b11397d35f713166c59db21496655b
lizparody <lizparody@gmail.com>
2017-08-30 18:18:10 -0500

    Using if/else statement

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import Chance from 'chance'; //generates realistic-looking random data, which means it can generate random first names, last names, sentences of text, 
+import Chance from 'chance'; //generates realistic-looking random data, which means it can generate random first names, last names, sentences of text,
 
 class Detail extends React.Component { //defines a new React component. React components can be big (like pages) or small (like a custom component to render breadcrumbs) and they are very flexible.
     render() { //starts the render() method of our component. This is called by React when the component needs to be drawn to the screen, and needs to return something that can be drawn in the browser.
-      return
-      {
+    return <p>
+    {
         chance.first() == 'John'
-        ? console.log('Got John')
-        : console.log('Got someone else')
-      }
-    } //inside {} bc it's JavaScript code rather than plain text.
+        ? 'Hello, John!'
+        : 'Hello, world!'
+    }
+    </p>;
+  }
 }
 
 export default Detail; //The "export" keyword means this component is being exposed to the rest of our app to use, and "default" means it's the only thing this class will expose.
