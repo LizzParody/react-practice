ca0b06555699bd4571cdafd11ef5e4a65f2e3862
lizparody <lizparody@gmail.com>
2017-08-30 17:45:18 -0500

    generating random first names

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Chance from 'chance'; //generates realistic-looking random data, which means it can generate random first names, last names, sentences of text, 
 
 class Detail extends React.Component { //defines a new React component. React components can be big (like pages) or small (like a custom component to render breadcrumbs) and they are very flexible.
     render() { //starts the render() method of our component. This is called by React when the component needs to be drawn to the screen, and needs to return something that can be drawn in the browser.
-      return <p>{this.props.message}</p>;
+      return <p>Hello, {chance.first()}!</p>;
     } //inside {} bc it's JavaScript code rather than plain text.
 }
 
