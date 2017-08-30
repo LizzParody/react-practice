f56b184a58db24ff2fc9743458958f0c3782e27d
lizparody <lizparody@gmail.com>
2017-08-30 18:37:00 -0500

    Render several elements at once

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -2,15 +2,12 @@ import React from 'react';
 import Chance from 'chance'; //generates realistic-looking random data, which means it can generate random first names, last names, sentences of text,
 
 class Detail extends React.Component { //defines a new React component. React components can be big (like pages) or small (like a custom component to render breadcrumbs) and they are very flexible.
-    render() { //starts the render() method of our component. This is called by React when the component needs to be drawn to the screen, and needs to return something that can be drawn in the browser.
-    return <p>
-    {
-        chance.first() == 'John'
-        ? 'Hello, John!'
-        : 'Hello, world!'
+    render() { //starts the render() method of our component. This is called by React when the component needs to be drawn to the screen, and needs to return something that can be drawn in the browser. It only returns ONE value
+        return (<div>
+          <p>Hello, {chance.first()}.</p>
+          <p>Youre from {chance.country({ full: true })}.</p>
+        </div>);
     }
-    </p>;
-  }
 }
 
 export default Detail; //The "export" keyword means this component is being exposed to the rest of our app to use, and "default" means it's the only thing this class will expose.
