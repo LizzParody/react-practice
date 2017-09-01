9a24d0888835d032899b944ad49e51ad8c5fd251
lizparody <lizparody@gmail.com>
2017-08-31 20:07:40 -0500

    state vs props

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -2,14 +2,23 @@ import React from 'react';
 import Chance from 'chance'; //generates realistic-looking random data, which means it can generate random first names, last names, sentences of text,
 
 class Detail extends React.Component { //defines a new React component. React components can be big (like pages) or small (like a custom component to render breadcrumbs) and they are very flexible.
+    constructor(props) {
+    super(props);
+
+    this.state = {
+        name: chance.first(),
+        country: chance.country({ full: true })
+        };
+    }
+
     buttonClicked(){
         this.forceUpdate(); // re-render our component so that a new name and place are generated
     }
 
     render() { //starts the render() method of our component. This is called by React when the component needs to be drawn to the screen, and needs to return something that can be drawn in the browser. It only returns ONE value.
         return (<div>
-            <p>Hello, {chance.first()}.</p>
-            <p>Youre from {chance.country({ full: true })}.</p>
+            <p>Hello, {this.state.name}.</p>
+            <p>Youre from {this.state.country}.</p>
             <button onClick={this.buttonClicked.bind(this)}>Meet someone New</button>
         </div>);
     } // bind() that lets you force JavaScript to use a definition of this you specify.
